d5ec8c99840437b4759a751c0f54b820aea5da04
Seleznev-Ilya <ziziron21@gmail.com>
2024-06-02 21:18:10 +0200

    Merge

--- src/core/components/common/Footer.tsx
+++ src/core/components/common/Footer.tsx
@@ -2,5 +2,5 @@ import useMainFont from "@/core/hooks/useMainFont";
 
 export const Footer = () => {
   const syne = useMainFont();
-  return <div className={syne}>Footer rg</div>;
+  return <div className={syne}>Footer 123 rg</div>;
 };
