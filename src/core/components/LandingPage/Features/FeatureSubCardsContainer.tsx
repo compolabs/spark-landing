aee07ead62925f5a2f0aa36187e71c4b8cb93a54
Seleznev-Ilya <ziziron21@gmail.com>
2024-06-07 17:02:32 +0200

    Delete useless component

--- src/core/components/LandingPage/Features/FeatureSubCardsContainer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import { CardsListType } from "./types";
-
-export const FeatureSubCardsContainer = ({ cardsList }: CardsListType) => {
-  return <div>FeatureSubCardsContainer</div>;
-};
